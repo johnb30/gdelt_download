66567b43622135ef3a6b07a0e6d523c1adfc314d
John B <john.b30@gmail.com>
2013-05-31 21:48:52 -0500

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
-gdelt_download_scripts
-======================
+###GDELT Download
 
-Set of scripts to aid in the download of the GDELT data files from gdelt.utdallas.edu
+The GDELT data is spread across multiple files, with a new file added each day.
+Downloading each and every file is not a fun endeavor. These scripts were 
+written in order to aid in the download of the GDELT data. The first script 
+`download_historical.py` is aimed at downloading the historical data, and the 
+previous daily updated. 
+
+A second script will soon be added. This script, `download_daily.py`, is aimed
+at downloading the new files that are uploaded to the GDELT website each day. 
+
+Each script implements a 30 second delay between each download in order to 
+avoid swamping the server.
+
+###`download_historical` Usage
+
+The script has three modes: `daily`, `single`, and `range`.
+
+*Note*: If you wish to use the `daily` mode, the `requests` and `lxml` libaries
+are necessary. You can install both using `pip install library_name`. The script 
+also makes use of `argparse`, which is included in the standard libary from
+Python 2.7+. If you are using an older version, it is necessary to install 
+`argparse` using `pip` or `easy_install`. 
+
+####Daily:
+
+The daily mode downloads the daily updates that are currently uploaded to the
+GDELT website.  
+
+#####Usage:
+
+`python download_historical.py daily -d ~/gdelt/ -U` 
+
+Where `-d` is the flag for the directory to which the files should be written,
+and `-U` is the optional flag indicating whether each downloaded file should
+be unzipped.
+
+####Single:
+
+The single mode downloads the updates for a single year.
+
+#####Usage:
+
+`python download_historical.py single -y 1979 -d ~/gdelt/ -U` 
+
+Where `-y` is the flag that indicates which year should be downloaded, `-d` 
+is the flag for the directory to which the files should be written, and `-U` 
+is the optional flag indicating whether each downloaded file should be unzipped.
+
+####Range:
+
+The range mode downloads the updates for a range years.
+
+#####Usage:
+
+`python download_historical.py range -y 1979-2012 -d ~/gdelt/ -U` 
+
+Where `-y` is the flag that indicates which years should be downloaded, `-d` 
+is the flag for the directory to which the files should be written, and `-U` 
+is the optional flag indicating whether each downloaded file should be unzipped.
+
---
+++ download_historical.py
@@ -0,0 +1,221 @@
+import os
+import math
+import time
+import urllib2
+import argparse
+import zipfile
+
+#Author: John Beieler, jub270@psu.edu
+#johnbeieler.org
+
+
+def get_historical_data(directory, year, unzip=False):
+    """
+    Function to download the historical files from the GDELT website.
+
+    Parameters
+    ----------
+
+    directory: String.
+               Directory to write the downloaded file.ing.
+
+    year: String or int.
+          Year of files to download.
+
+    unzip: Boolean.
+           Argument that indicates whether or not to unzip the downloaded
+           file. Defaults to False.
+
+    """
+    if int(year) < 2006:
+            to_get = '{}'.format(year)
+            print 'Processing {}'.format(to_get)
+            url = 'http://gdelt.utdallas.edu/data/backfiles/{}.zip'.format(to_get)
+            written_file = _download_chunks(directory, url)
+    elif int(year) >= 2006:
+        for i in range(01, 13):
+            if i < 10:
+                month = '0{}'.format(i)
+            else:
+                month = str(i)
+            to_get = '{}{}'.format(year, month)
+            print 'Processing {}'.format(to_get)
+            url = 'http://gdelt.utdallas.edu/data/backfiles/{}.zip'.format(to_get)
+            written_file = _download_chunks(directory, url)
+    else:
+        print "That's not a valid year!"
+    if unzip:
+        _unzip_file(directory, written_file)
+    time.sleep(30)
+
+
+def get_historical_daily(directory, unzip=False):
+    """
+    Function to download the daily update files from the GDELT website.
+
+    Parameters
+    ----------
+
+    directory: String.
+               Directory to write the downloaded file.ing.
+
+    unzip: Boolean.
+           Argument that indicates whether or not to unzip the downloaded
+           file. Defaults to False.
+
+    """
+    urls = _get_links()
+    for url in urls:
+        written_file = _download_chunks(directory, url)
+        if unzip:
+            _unzip_file(directory, written_file)
+        time.sleep(30)
+
+
+def _get_links():
+    """
+    Private function to obtain the links for the daily update files from
+    the GDELT website. Requires requests and lxmlself.
+    """
+    import requests
+    import lxml.html as lh
+    url = 'http://gdelt.utdallas.edu/data/dailyupdates/?O=D'
+    page = requests.get(url)
+    text = lh.fromstring(page.content)
+    urls = text.xpath('//a/@href')
+    urls = [url for url in urls if '.zip' in url]
+
+    return urls
+
+
+def _unzip_file(directory, zipped_file):
+    """
+    Private function to unzip a zipped file that was downloaded.
+
+    Parameters
+    ----------
+
+    directory: String.
+               Directory to write the unzipped file.
+
+    zipped_file: String.
+                 Filepath of the zipped file to unzip.
+
+    """
+    print 'Unzipping {}'.format(zipped_file)
+    z = zipfile.ZipFile(zipped_file)
+    for name in z.namelist():
+        f = z.open(name)
+        out_path = os.path.join(directory, f)
+        with open(out_path, 'w') as out_file:
+            out_file.write(f.read())
+
+
+def _download_chunks(directory, url):
+#Thanks to https://gist.github.com/gourneau/1430932
+    """
+    Private function to download a zipped file in chunks.
+
+    Parameters
+    ----------
+
+    directory: String.
+               Directory to write the downloaded file.
+
+    url: String.
+         Url of the file to download.
+
+    """
+
+    base_file = os.path.basename(url)
+
+    temp_path = directory
+    try:
+        file = os.path.join(temp_path, base_file)
+
+        req = urllib2.urlopen(url)
+        total_size = int(req.info().getheader('Content-Length').strip())
+        downloaded = 0
+        CHUNK = 256 * 10240
+        with open(file, 'wb') as fp:
+            while True:
+                chunk = req.read(CHUNK)
+                downloaded += len(chunk)
+                prog = math.floor((downloaded / total_size) * 100)
+                if prog == 0.0:
+                    pass
+                else:
+                    print prog
+                if not chunk:
+                    break
+                fp.write(chunk)
+    except urllib2.HTTPError, e:
+        print "HTTP Error:", e.code, url
+        return False
+    except urllib2.URLError, e:
+        print "URL Error:", e.reason, url
+        return False
+
+    return file
+
+if __name__ == '__main__':
+    print 'Running...'
+    aparse = argparse.ArgumentParser()
+
+    sub_parse = aparse.add_subparsers(dest='command_name')
+
+    daily_command = sub_parse.add_parser('daily', help="""Download the old
+                                         daily update files.""",
+                                         description="""Download the old
+                                         daily update files.""")
+    daily_command.add_argument('-d', '--directory', help="""Path of directory
+                               for file download""")
+    daily_command.add_argument('-U', '--unzip', '--unzip', action='store_true',
+                               default=False, help="""Boolean flag indicating
+                               whether or not to unzip the downloaded
+                               files.""")
+
+    single_command = sub_parse.add_parser('single', help="""Download a single
+                                          year.""",
+                                          description="""Download a single
+                                          year.""")
+    single_command.add_argument('-y', '--year', help='Year to download')
+    single_command.add_argument('-d', '--directory', help="""Path of directory
+                                for file download""")
+    single_command.add_argument('-U', '--unzip', '--unzip', action='store_true',
+                                default=False, help="""Boolean flag indicating
+                                whether or not to unzip the downloaded
+                                files.""")
+
+    range_command = sub_parse.add_parser('range', help="""Download a range
+                                         of years.""",
+                                         description="""Download a range
+                                         of years.""")
+    range_command.add_argument('-y', '--year', help="""Years to download, e.g.
+                               1979-1981""")
+    range_command.add_argument('-d', '--directory', help="""Path of directory
+                               for file download""")
+    range_command.add_argument('-U', '--unzip', '--unzip', action='store_true',
+                               default=False, help="""Boolean flag indicating
+                               whether or not to unzip the downloaded
+                               files.""")
+
+    args = aparse.parse_args()
+    directory = args.directory
+
+    if args.command_name == 'daily':
+        get_historical_daily(directory, args.unzip)
+    elif args.command_name == 'single':
+        get_historical_data(directory, args.single, args.unzip)
+    elif args.command_name == 'range':
+        try:
+            begin, end = args.range.split('-')
+        except Exception, e:
+            print 'Error {}. Please enter a valid range, e.g. 1979-1980'.format(e)
+        try:
+            begin = int(begin)
+            end = int(end)
+        except Exception, e:
+            print 'Error {}. Please enter a valid range, e.g. 1979-1980.'.format(e)
+        for y in range(begin, end+1):
+            get_historical_data(directory, y, args.unzip)
